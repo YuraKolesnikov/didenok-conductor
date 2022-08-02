5c01fb2ecceecfde79dbc1492afd7645849d1f1a
Yura Kolesnikov <jurijs.kolesnikovs@gmail.com>
2022-08-03 00:33:27 +0700

    Fixes

--- components/ContentBox/ContentBox.jsx
+++ components/ContentBox/ContentBox.jsx
@@ -5,27 +5,35 @@ import css from './ContentBox.module.scss'
 
 export const UNDERSCORE_COLORS = {
 	WHITE: 'white',
-	ACCENT_2: 'accent_2'
+	ACCENT_2: 'accent_2',
 }
 
-UNDERSCORE_COLORS.LIST = [
-	UNDERSCORE_COLORS.WHITE,
-	UNDERSCORE_COLORS.ACCENT_2,
-]
+UNDERSCORE_COLORS.LIST = [UNDERSCORE_COLORS.WHITE, UNDERSCORE_COLORS.ACCENT_2]
 
-const ContentBox = ({ title, text, underscoreColor, backgroundColor, backgroundImage, className }) => {
+const ContentBox = ({
+	title,
+	text,
+	underscoreColor,
+	backgroundColor,
+	backgroundImage,
+	className,
+}) => {
 	const boxClassList = classNames({
 		[css.box]: true,
 		[css[`bg_${backgroundColor}`]]: true,
-		[className]: !!className
+		[className]: !!className,
 	})
 
 	return (
 		<div className={boxClassList}>
 			<div className={css.content}>
-				<h4 className={css.title}>{title}<span className={css[underscoreColor]}>_</span></h4>
+				<h4 className={css.title}>
+					{title}
+					<span className={css[underscoreColor]}>_</span>
+				</h4>
 				<p className={css.text}>{text}</p>
 			</div>
+			{!!backgroundImage && <img className={css.bg} src={backgroundImage} />}
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -3,134 +3,134 @@ import classNames from 'classnames'
 import { Element } from 'react-scroll'
 
 import {
-  Border,
-  Container,
-  ContentBox,
-  Logo,
-  Hero,
-  LargeBox,
-  SectionContactUs,
-  ClientsSlider,
+	Border,
+	Container,
+	ContentBox,
+	Logo,
+	Hero,
+	LargeBox,
+	SectionContactUs,
+	ClientsSlider,
 } from '../components'
 
 import css from './index.module.scss'
 
 const LARGE_BOXES = [
-  {
-    position: 1,
-    backgroundImg: '/assets/large_boxes/box_1.svg',
-    mobileBackgroundImg: '/assets/large_boxes/box_1_mobile.svg',
-    title: 'Большой опыт работы',
-    text: 'С разными сферами бизнеса и личными брендами'
-  },
-  {
-    position: 2,
-    backgroundImg: '/assets/large_boxes/box_2.svg',
-    mobileBackgroundImg: '/assets/large_boxes/box_2_mobile.svg',
-    title: 'Детально и погруженно',
-    text: 'Подходим к продвижению и настройке рекламы'
-  },
-  {
-    position: 3,
-    backgroundImg: '/assets/large_boxes/box_3.svg',
-    mobileBackgroundImg: '/assets/large_boxes/box_3_mobile.svg',
-    title: 'Собственная команда специалистов',
-    text: 'В штате, закроем все задачи по проекту без привлечения фрилансеров.'
-  },
-  {
-    position: 4,
-    backgroundImg: '/assets/large_boxes/box_4.png',
-    mobileBackgroundImg: '/assets/large_boxes/box_4.png',
-    title: 'Глубокая аналитика',
-    text: 'В сферах креатива и стратегии'
-  },
+	{
+		position: 1,
+		backgroundImg: '/assets/large_boxes/box_1.svg',
+		mobileBackgroundImg: '/assets/large_boxes/box_1_mobile.svg',
+		title: 'Большой опыт работы',
+		text: 'С разными сферами бизнеса и личными брендами',
+	},
+	{
+		position: 2,
+		backgroundImg: '/assets/large_boxes/box_2.svg',
+		mobileBackgroundImg: '/assets/large_boxes/box_2_mobile.svg',
+		title: 'Детально и погруженно',
+		text: 'Подходим к продвижению и настройке рекламы',
+	},
+	{
+		position: 3,
+		backgroundImg: '/assets/large_boxes/box_3.svg',
+		mobileBackgroundImg: '/assets/large_boxes/box_3_mobile.svg',
+		title: 'Собственная команда специалистов',
+		text: 'В штате, закроем все задачи по проекту без привлечения фрилансеров.',
+	},
+	{
+		position: 4,
+		backgroundImg: '/assets/large_boxes/box_4.png',
+		mobileBackgroundImg: '/assets/large_boxes/box_4.png',
+		title: 'Глубокая аналитика',
+		text: 'В сферах креатива и стратегии',
+	},
 ]
 
 export default function Home() {
-
-  return (
-    <>
-      <Border className={css.underNav} />
-      <Container className={css.container}>
-        <div className={css.logoWrapper}>
-          <Logo />
-        </div>
-        <Hero />
-      </Container>
-      <Border />
-      <Container className={css.container}>
-        <Element id="services" name="services">
-          <div className={css.grid}>
-            <ContentBox
-              title="Strategy"
-              text="Разрабатываем коммуникационные и SMM-стратегии для брендов в DIGITAL."
-              underscoreColor="white"
-              backgroundColor="accent_2"
-            />
-            <ContentBox
-              title="Creative"
-              text="Креативим, пишем сценарии, разрабатываем спецпроекты."
-              underscoreColor="accent_2"
-              backgroundColor="black"
-            />
-            <ContentBox
-              className={css.tall}
-              title="SMM"
-              text="Комплексное ведение соцсетей бренда: от контент-плана до продвижения."
-              underscoreColor="accent_2"
-              backgroundColor="black"
-            />
-            <ContentBox
-              title="Production"
-              text="Продюсируем съёмки любой сложности.Фотографируем, снимаем, монтируем."
-              underscoreColor="accent_2"
-              backgroundColor="white"
-            />
-            <ContentBox
-              title="Promo"
-              text="Протестируем и запустим только эффективные инструменты продвижения: таргетинг, посевы, инфлюенсеры, контекстная и SEO-реклама."
-              underscoreColor="accent_2"
-              backgroundColor="black"
-            />
-            <ContentBox
-              title="Design"
-              text="Дизайн у нас в крови: от простой графики для соцсетей до сложных 3D-роликов."
-              underscoreColor="accent_2"
-              backgroundColor="white"
-            />
-            <ContentBox
-              title="Digital Pr"
-              text="Занимаемся корпоративным PR во всех его проявлениях: абонентский, антикризисный, репутационный маркетинг и спецпроекты."
-              underscoreColor="accent_2"
-              backgroundColor="black"
-            />
-            <ContentBox
-              title="App / Web Production"
-              text="Cоздаем лендинги и сайты, разрабатываем приложения и ботов."
-              underscoreColor="accent_2"
-              backgroundColor="white"
-            />
-          </div>
-        </Element>
-      </Container>
-      <Border />
-      <Container className={css.largeBoxGrid}>
-        {LARGE_BOXES.map(box => (
-          <LargeBox {...box} />
-        ))}
-        <Border className={css.border_1} />
-        <Border className={css.border_2} />
-        <Border className={css.border_3} />
-        <Border className={css.border_4} />
-      </Container>
-      <Border />
-      <Container
-        className={classNames(css.container, css.clientsSlider)}>
-        <ClientsSlider />
-      </Container>
-      <Border />
-      <SectionContactUs />
-      <Border />
-    </>
-  )
+	return (
+		<>
+			<Border className={css.underNav} />
+			<Container className={css.container}>
+				<div className={css.logoWrapper}>
+					<Logo />
+				</div>
+				<Hero />
+			</Container>
+			<Border />
+			<Container className={css.container}>
+				<Element id="services" name="services">
+					<div className={css.grid}>
+						<ContentBox
+							title="Strategy"
+							text="Разрабатываем коммуникационные и SMM-стратегии для брендов в DIGITAL."
+							underscoreColor="white"
+							backgroundColor="accent_2"
+						/>
+						<ContentBox
+							title="Creative"
+							text="Креативим, пишем сценарии, разрабатываем спецпроекты."
+							underscoreColor="accent_2"
+							backgroundColor="black"
+						/>
+						<ContentBox
+							className={css.tall}
+							title="SMM"
+							text="Комплексное ведение соцсетей бренда: от контент-плана до продвижения."
+							underscoreColor="accent_2"
+							backgroundColor="black"
+							backgroundImage="/assets/content_boxes/img_1.png"
+						/>
+						<ContentBox
+							title="Production"
+							text="Продюсируем съёмки любой сложности.Фотографируем, снимаем, монтируем."
+							underscoreColor="accent_2"
+							backgroundColor="white"
+						/>
+						<ContentBox
+							title="Promo"
+							text="Протестируем и запустим только эффективные инструменты продвижения: таргетинг, посевы, инфлюенсеры, контекстная и SEO-реклама."
+							underscoreColor="accent_2"
+							backgroundColor="black"
+						/>
+						<ContentBox
+							title="Design"
+							text="Дизайн у нас в крови: от простой графики для соцсетей до сложных 3D-роликов."
+							underscoreColor="accent_2"
+							backgroundColor="black"
+							backgroundImage="/assets/content_boxes/img_2.png"
+						/>
+						<ContentBox
+							title="Digital Pr"
+							text="Занимаемся корпоративным PR во всех его проявлениях: абонентский, антикризисный, репутационный маркетинг и спецпроекты."
+							underscoreColor="accent_2"
+							backgroundColor="black"
+						/>
+						<ContentBox
+							title="App / Web Production"
+							text="Cоздаем лендинги и сайты, разрабатываем приложения и ботов."
+							underscoreColor="accent_2"
+							backgroundColor="white"
+						/>
+					</div>
+				</Element>
+			</Container>
+			<Border />
+			<Container className={css.largeBoxGrid}>
+				{LARGE_BOXES.map(box => (
+					<LargeBox {...box} />
+				))}
+				<Border className={css.border_1} />
+				<Border className={css.border_2} />
+				<Border className={css.border_3} />
+				<Border className={css.border_4} />
+			</Container>
+			<Border />
+			<Container className={classNames(css.container, css.clientsSlider)}>
+				<ClientsSlider />
+			</Container>
+			<Border />
+			<SectionContactUs />
+			<Border />
+		</>
+	)
 }
